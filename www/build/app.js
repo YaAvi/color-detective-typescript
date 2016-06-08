0a26dbb4d57c121e45baa71ac1658bc7404b4d32
Avi Y <aviyaakov1@gmail.com>
2016-06-08 18:09:17 +0300

    removed build folder

--- www/build/app.js
+++
@@ -1,7 +0,0 @@
-var cda;
-(function (cda) {
-    angular.module('cda', ['ngAnimate', 'ngCordova', 'pascalprecht.translate', 'mn']);
-    document.addEventListener('deviceready', function () {
-        angular.bootstrap(document, ['cda']);
-    }, false);
-})(cda || (cda = {}));
